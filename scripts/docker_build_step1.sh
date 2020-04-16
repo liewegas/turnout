331ce8260d7a1e3d1f72f7a909356b5748b6fc0e
Nick Catalano <nickcatal@gmail.com>
2020-04-16 15:31:58 -0400

    Install wait-for-it in dev only

--- scripts/docker_build_step1.sh
+++ scripts/docker_build_step1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
-apt-get install -y nodejs wait-for-it
+apt-get install -y nodejs
 
 pip install -r requirements.txt
 npm install
